8f1273d015754e204a63b97a0b2802632c5d3a33
Zhuo Hu <943181782@qq.com>
2019-08-20 18:07:37 +0800

    delete

--- server/package/class/orders.js
+++
@@ -1,8 +0,0 @@
-import util from '../class/util.js';
-class Orders {
-    getOrdersList(index) {
-
-        return util.getMsg("http://localhost:8080/getOrderList", index)
-    }
-}
-export default Orders;
